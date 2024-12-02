2494c81a3e6291492a6314151a205f7fe9fc0571
Mujeeb Adeniji <madeniji@mujeebs-mbp.home>
2024-12-02 22:27:24 +0000

    add

--- infra/terraform/aws-apigateway-s3/outputs.tf
+++ infra/terraform/aws-apigateway-s3/outputs.tf
@@ -1,11 +1,11 @@
-# output "role_apigateway_arn" {
-#   value = aws_iam_role.spain_sub_apigateway_s3_api_role.arn
-# }
+output "role_apigateway_arn" {
+  value = aws_iam_role.spain_sub_apigateway_s3_api_role.arn
+}
 
-# output "role_apigateway_s3_policy_arn" {
-#   value = aws_iam_policy.spain_sub_apigateway_s3_iam_policy.arn
-# }
+output "role_apigateway_s3_policy_arn" {
+  value = aws_iam_policy.spain_sub_apigateway_s3_iam_policy.arn
+}
 
-# output "invoke_url" {
-#   value = aws_api_gateway_stage.spain_sub_apigateway_stage.invoke_url
-# }
+output "invoke_url" {
+  value = aws_api_gateway_stage.spain_sub_apigateway_stage.invoke_url
+}
